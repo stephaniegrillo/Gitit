65be8b4ba6f6d22aee559da8e2fdf96b2bfab082
Stephanie Grillo <stephanie.grillo@ymail.com>
2017-06-15 18:13:45 -0400

    Update my website

---
+++ errors.rb
@@ -0,0 +1,43 @@
+# class Table
+#   attr_accessor :num_legs
+#
+#   def initialize (num_legs)
+#     if num_legs > 0
+#       @num_legs = num_legs
+#     else
+#       # @num_legs = 4
+#       # puts "Please don't do that"
+#       # return Exception.new
+#       # return "it doesn't matter what you return from initialize"
+#       # asdf
+#       raise ArgumentError, "Invalid number of legs"
+#     end
+#   end
+# end
+#
+# begin
+#   table = Table.new(-1)
+# rescue ArgumentError
+#   puts "I'm sorry for passing bad arguments"
+# rescue RuntimeError
+#
+# end ArgumentError
+# puts "Why?!?"
+# end
+#
+# p table.num_legs
+
+def a
+  begin
+    begin
+  rescue 
+    rescue ExceptionName
+
+    end
+
+
+def d
+  a
+rescue
+  puts "Saved the world!"
+end
--- server.rb
+++ server.rb
@@ -1,9 +1,36 @@
+# require 'sinatra'
+#
+# get '/' do
+#   "Hello World"
+# end
+#
+# get '/wyncode' do
+#   "<strong>Hello</strong><em>world</em>"
+# end
+
+# number 1 Hello index
+#
+#
+# require 'sinatra'
+#
+# get '/' do
+#  File.new("public/hello.txt").readlines
+# end
+
+question 3 
+
 require 'sinatra'
+require 'nokogiri'
+require 'httparty'
 
 get '/' do
-  "Hello World"
-end
-
-get '/wyncode' do
-  "<strong>Hello</strong><em>world</em>"
+  url = "https://miami.craigslist.org/search/mdc/sof"
+  response = HTTParty.get(url)
+  doc = Nokogiri::HTML(response.body)
+  titles =doc.css(".result-title")
+  dates = doc.css(".result-date")
+  count = 0
+  titles.map do |title|
+  "<p>july 1 - #{title.to_str}</p>"
+  end
 end
